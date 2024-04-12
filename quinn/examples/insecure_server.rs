18165c5e348ef2b912b0a0ed32773c0b7e83bf6b
YutongDu <yutong.du@aalto.fi>
2024-04-12 18:19:43 +0300

    增加了了不同大小的文件

--- quinn/examples/insecure_server.rs
+++ quinn/examples/insecure_server.rs
@@ -37,8 +37,8 @@ struct Opt {
     #[clap(long = "stateless-retry")]
     stateless_retry: bool,
     /// Address to listen on
-    // #[clap(long = "listen", default_value = "[::1]:4433")] // loopback addr
-    #[clap(long = "listen", default_value = "10.0.0.1:4433")] // mininet host addr
+    #[clap(long = "listen", default_value = "[::1]:4433")] // loopback addr
+    // #[clap(long = "listen", default_value = "10.0.0.1:4433")] // mininet host addr
     listen: SocketAddr,
 }
 
@@ -152,7 +152,8 @@ async fn run(options: Opt) -> Result<()> {
     // let addr = "[::1]:4433".parse().unwrap(); // loopback addr
     // let (endpoint, _server_cert) = make_server_endpoint(addr).unwrap(); // loopback addr
 
-    let addr: SocketAddr = "10.0.0.1:4433".parse()?;  // mininet host addr
+    let addr = "[::1]:4433".parse().unwrap(); // loopback addr
+    // let addr: SocketAddr = "10.0.0.1:4433".parse()?;  // mininet host addr
     let (endpoint, _server_cert) = make_server_endpoint(addr).unwrap(); // mininet host addr
 
     // accept a single connection
